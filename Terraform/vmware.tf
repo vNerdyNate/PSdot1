b54322d8fb3d2af43df6065b8c8936a397e4a9d0
Nathan Bennett <vnerdynate@MBP.local>
2020-03-28 10:36:19 -0500

    vmware updates

--- Terraform/vmware.tf
+++ Terraform/vmware.tf
@@ -26,8 +26,62 @@ data "vsphere_network" "network" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-resource "vsphere_virtual_machine" "vm" {
-  name             = "terraform-test"
+resource "vsphere_virtual_machine" "vm4" {
+  name             = "terraform-test1"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 2
+  memory   = 1024
+  guest_id = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
+resource "vsphere_virtual_machine" "vm3" {
+  name             = "terraform-test2"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 2
+  memory   = 1024
+  guest_id = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
+resource "vsphere_virtual_machine" "vm2" {
+  name             = "terraform-test3"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 2
+  memory   = 1024
+  guest_id = "other3xLinux64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.network.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+}
+resource "vsphere_virtual_machine" "vm1" {
+  name             = "terraform-test4"
   resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
 
